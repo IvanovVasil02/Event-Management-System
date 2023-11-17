b1e10fcc4cae0a20dda497adde0cb6611871ffb0
IvanovVasil02 <ivanov.vasil235@gmail.com>
2023-11-17 09:59:57 +0100

    first commit

---
+++ src/test/java/ivanovvasil/u5d5w5Project/U5D5W5ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package ivanovvasil.u5d5w5Project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U5D5W5ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
